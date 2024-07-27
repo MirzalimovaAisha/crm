ef901e28c683b55b8fb039dc3cd622e47fd4fe08
Aisha <mir31090@gmail.com>
2024-07-27 19:29:54 +0900

    initial

--- src/components/budget/budget.jsx
+++ src/components/budget/budget.jsx
@@ -20,85 +20,113 @@ import BudgetCategory from "./budget-Category";
 import BedgetExpense from "./budget-Expense";
 
 const BudgetComponent = () => {
-  const [value, setValue] = React.useState("1");
+    const [value, setValue] = React.useState("1");
 
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
 
-  return (
-    <div style={{ display: "flex", justifyContent: "center" }}>
-      <GroupsContainer>
-        <Box sx={{ width: "100%", typography: "body1" }}>
-          <TabContext value={value}>
-            <div>
-              <BudgetTopTextContainer>
-                <Box
-                  sx={{
-                    borderBottom: 1,
-                    borderColor: "divider",
-                    '.css-1aquho2-MuiTabs-indicator': {
-                      display: "none",
-                    },
-                    '.css-heg063-MuiTabs-flexContainer': {
-                      height: "44px",
-                    },
-                    '.css-jpln7h-MuiTabs-scroller': {
-                      border: "2px solid var(--600, #2C2669)",
-                      borderRadius: "6px",
-                      transition: "all 0.2s",
-                    },
-                    '.css-1h9z7r5-MuiButtonBase-root-MuiTab-root': {
-                      width: "187px",
-                      color: "#2C2669 !important",
-                      fontSize: "14px !important",
-                      fontWeight: "500 !important",
-                      transition: "all 0.2s",
-                      fontFamily: "Public Sans !important",
-                      textTransform: "none !important",
-                      letterSpacing: "0 !important",
-                      minHeight: "auto !important",
-                    },
-                    '.css-1h9z7r5-MuiButtonBase-root-MuiTab-root.Mui-selected': {
-                      transition: "all 0.2s",
-                      background: "#2C2669 !important",
-                      color: "#fff !important",
-                    },
-                    '.css-13xfq8m-MuiTabPanel-root': {
-                      padding: "30px 0px 0px !important",
-                    },
-                  }}
-                >
-                  <TabList
-                    onChange={handleChange}
-                    aria-label="lab API tabs example"
-                    className="tab-list"
-                  >
-                    <Tab label="Income" value="1" className="tab" style={{borderRight:"2px solid #2C2669"}}/>
-                    <Tab label="Category" value="2" className="tab" style={{borderRight:"2px solid #2C2669"}}/>
-                    <Tab label="Expense" value="3" className="tab" />
-                  </TabList>
+    return (
+        <div style={{ display: "flex", justifyContent: "center" }}>
+            <GroupsContainer>
+                <Box sx={{ width: "100%", typography: "body1" }}>
+                    <TabContext value={value}>
+                        <div>
+                            <BudgetTopTextContainer>
+                                <Box>
+                                    <TabList
+                                        onChange={handleChange}
+                                        aria-label="lab API tabs example"
+                                        className="tab-list"
+                                        sx={{
+                                            border: "2px solid #2C2669",
+                                            borderRadius: "6px",
+                                            height: "44px",
+                                            ".MuiTabs-indicator": {
+                                                display: "none",
+                                            },
+                                        }}
+                                    >
+                                        <Tab
+                                            label="Income"
+                                            value="1"
+                                            className="tab"
+                                            sx={{
+                                                borderRight:
+                                                    "2px solid #2C2669",
+                                                width: "187px",
+                                                color: "#2C2669",
+                                                textTransform: "none",
+                                                "&.Mui-selected": {
+                                                    background: "#2C2669",
+                                                    color: "#fff",
+                                                },
+                                            }}
+                                        />
+                                        <Tab
+                                            label="Category"
+                                            value="2"
+                                            className="tab"
+                                            sx={{
+                                                color: "#2C2669",
+                                                borderRight:
+                                                    "2px solid #2C2669",
+                                                textTransform: "none",
+                                                width: "187px",
+                                                "&.Mui-selected": {
+                                                    background: "#2C2669",
+                                                    color: "#fff",
+                                                },
+                                            }}
+                                        />
+                                        <Tab
+                                            label="Expense"
+                                            value="3"
+                                            className="tab"
+                                            sx={{
+                                                width: "187px",
+                                                textTransform: "none",
+                                                color: "#2C2669",
+                                                "&.Mui-selected": {
+                                                    background: "#2C2669",
+                                                    color: "#fff",
+                                                },
+                                            }}
+                                        />
+                                    </TabList>
+                                </Box>
+                                <BudgetData>
+                                    <img src={data} alt="" />
+                                    <div>July 23, 2023 - August 23, 2023</div>
+                                </BudgetData>
+                            </BudgetTopTextContainer>
+                        </div>
+                        <TabPanel
+                            value="1"
+                            className="tab-panel"
+                            sx={{ padding: "0px" }}
+                        >
+                            <BudgetIncome />
+                        </TabPanel>
+                        <TabPanel
+                            value="2"
+                            className="tab-panel"
+                            sx={{ padding: "0px" }}
+                        >
+                            <BudgetCategory />
+                        </TabPanel>
+                        <TabPanel
+                            value="3"
+                            className="tab-panel"
+                            sx={{ padding: "0px" }}
+                        >
+                            <BedgetExpense />
+                        </TabPanel>
+                    </TabContext>
                 </Box>
-                <BudgetData>
-                  <img src={data} alt="" />
-                  <div>July 23, 2023 - August 23, 2023</div>
-                </BudgetData>
-              </BudgetTopTextContainer>
-            </div>
-            <TabPanel value="1" className="tab-panel" sx={{padding:"0px"}}>
-              <BudgetIncome />
-            </TabPanel>
-            <TabPanel value="2" className="tab-panel"  sx={{padding:"0px"}}>
-              <BudgetCategory />
-            </TabPanel>
-            <TabPanel value="3" className="tab-panel" sx={{padding:"0px"}}>
-              <BedgetExpense />
-            </TabPanel>
-          </TabContext>
-        </Box>
-      </GroupsContainer>
-    </div>
-  );
+            </GroupsContainer>
+        </div>
+    );
 };
 
 export default BudgetComponent;
